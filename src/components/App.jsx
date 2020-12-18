356b9f1dfb64efc1434fe87a0231b83ab26f3154
bjimingtang <brendan.jiming.tang@gmail.com>
2020-12-18 12:59:55 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from "../emojipedia.js";
+import Entry from "./Entry.jsx";
+
+function createEntry(entry) {
+  return (
+    <Entry
+      key={entry.id}
+      id={entry.id}
+      emoji={entry.emoji}
+      name={entry.name}
+      meaning={entry.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
